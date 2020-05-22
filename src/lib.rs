72579f08ddf950869eb1f42c74afe79c60a52845
YUJI <mail@yuji.ne.jp>
2020-05-22 02:48:34 +0000

    We'll send 404 response!!!

--- src/lib.rs
+++ src/lib.rs
@@ -26,38 +26,41 @@ pub fn parser(json: &str) -> String {
 
     let json: Value = serde_json::from_str(json).unwrap();
 
-    let title = format!(
-        "{} (@{}) from instagram",
-        json["graphql"]["user"]["full_name"].as_str().unwrap(),
-        json["graphql"]["user"]["username"].as_str().unwrap()
-    );
-
     let contents = json["graphql"]["user"]["edge_owner_to_timeline_media"]["edges"]
         .as_array()
         .unwrap();
     for content in contents {
         let content = &content["node"];
 
+        let title = match content["accessibility_caption"].as_str() {
+            Some(caption) => caption.to_string(),
+            None => format!(
+                "Photo by {}{}",
+                json["graphql"]["user"]["full_name"].as_str().unwrap(),
+                match content["location"]["name"].as_str() {
+                    Some(location) => format!(" at {}", location),
+                    None => "".to_string(),
+                }
+            ),
+        };
+
         let photos = match content["edge_sidecar_to_children"]["edges"].as_array() {
-            Some(medias) => {
-                let mut photos: Vec<String> = vec![];
-                for media in medias {
-                    photos.push(format!(
+            Some(medias) => medias
+                .iter()
+                .map(|media| {
+                    format!(
                         "<img src=\"{}\" />",
                         media["node"]["display_url"].as_str().unwrap()
-                    ));
-                }
-                photos
-            }
+                    )
+                })
+                .collect(),
             None => vec![format!(
                 "<img src=\"{}\" />",
                 content["display_url"].as_str().unwrap()
             )],
         };
-
         let desc = format!(
-            "<p>{}</p>
-            {}",
+            "<p>{}</p>{}",
             content["edge_media_to_caption"]["edges"][0]["node"]["text"]
                 .as_str()
                 .unwrap()
@@ -72,8 +75,8 @@ pub fn parser(json: &str) -> String {
         let date = Utc.timestamp(date, 0);
 
         let item = ItemBuilder::default()
+            .title(title)
             .description(desc)
-            .title(title.to_owned())
             .link(link)
             .pub_date(date.to_rfc2822())
             .build()
@@ -81,7 +84,15 @@ pub fn parser(json: &str) -> String {
         items.push(item);
     }
 
-    let link = format!("https://www.instagram.com/{}", json["graphql"]["user"]["username"].as_str().unwrap());
+    let title = format!(
+        "{} (@{}) from instagram",
+        json["graphql"]["user"]["full_name"].as_str().unwrap(),
+        json["graphql"]["user"]["username"].as_str().unwrap()
+    );
+    let link = format!(
+        "https://www.instagram.com/{}",
+        json["graphql"]["user"]["username"].as_str().unwrap()
+    );
     let description = json["graphql"]["user"]["biography"].as_str().unwrap();
     let image = format!(
         "{}",
@@ -91,16 +102,16 @@ pub fn parser(json: &str) -> String {
     );
 
     let image = ImageBuilder::default()
-        .url(&image)
         .title(&title)
         .link(&link)
+        .url(image)
         .build()
         .unwrap();
 
     ChannelBuilder::default()
         .title(&title)
-        .link(&link)
         .description(description)
+        .link(&link)
         .image(image)
         .items(items)
         .build()
--- worker/worker.js
+++ worker/worker.js
@@ -2,15 +2,25 @@ addEventListener('fetch', event => {
   event.respondWith(handleRequest(event.request))
 })
 
+async function create404Responce() {
+  const res = await fetch(`https://yuji.ne.jp/404.html`);
+  return new Response(await res.text(), {
+    status: 404,
+    headers: { "Content-Type": 'text/html; charset="UTF-8"' }
+  });
+}
+
 /**
  * Fetch and log a request
  * @param {Request} request
  */
 async function handleRequest(request) {
   const url = new URL(request.url);
+  if (url.pathname.slice(-4) != ".xml") { return await create404Responce(); }
   const username = url.pathname.slice(11, -4);
 
   const res = await fetch(`https://www.instagram.com/${username}/?__a=1`);
+  if (!res.ok) { return await create404Responce(); }
   const json = await res.text();
 
 
